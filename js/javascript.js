19a5e8e9ba7948f63da11c1e42d47488a1e509e7
mkharodia11 <kharodia_14@hotmail.com>
2017-11-23 00:18:05 +0000

    Inserted Javascript tag and Javascript code along with changes in CSS

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,25 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+(function () {
+	
+	let menuButton = document.getElementbyId ("mainMenu");
+	let navMenu = document.getElementById ("nav-menu");
+	
+	
+	menuButton.addEventListener("click", toggleMenu);
+	
+	
+	let toggle = false; //hidden at first
+	function toggleMenu() {
+		if (toggle) { //if menu is visible
+		navMenu.classList.remove("displayMenu"), //hide the menu
+		toggle = false
+		}
+		else { // false: it's hidden
+		navMenu.classList.add("displayMenu"), // show the menu
+		toggle = true
+		}
+	}
+
+}) ();
+	
+	
