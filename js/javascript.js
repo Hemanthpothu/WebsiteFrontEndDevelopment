07275b789088446d2473a70a282cf10142788970
mkharodia11 <kharodia_14@hotmail.com>
2017-11-23 15:29:45 +0000

    Responsive nav bar works and phone logo updated

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,19 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-(function () {
+(function(){
 	
-	let menuButton = document.getElementbyId ("mainMenu");
+	let menuButton = document.getElementById ("mainmenu");
 	let navMenu = document.getElementById ("nav-menu");
 	
-	
-	menuButton.addEventListener("click", toggleMenu);
+	menuButton.addEventListener("click", togglemenu);
 	
 	
 	let toggle = false; //hidden at first
-	function toggleMenu() {
+	function togglemenu() {
 		if (toggle) { //if menu is visible
-		navMenu.classList.remove("displayMenu"), //hide the menu
+		navMenu.classList.remove("displaymenu"), //hide the menu
 		toggle = false
 		}
 		else { // false: it's hidden
-		navMenu.classList.add("displayMenu"), // show the menu
+		navMenu.classList.add("displaymenu"), // show the menu
 		toggle = true
 		}
 	}
