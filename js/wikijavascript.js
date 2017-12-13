09ca3a88492d1c1553539a6a4c52a23355514ab0
mkharodia11 <p13233113@GWHXX280WIN018W.lec-admin.dmu.ac.uk>
2017-12-13 22:41:32 +0000

    Inserted Wiki API in the latest tech page

--- js/wikijavascript.js
+++ js/wikijavascript.js
@@ -1,120 +1,3 @@
-(function(){
-	
-	let menuButton = document.getElementById ("mainmenu");
-	let navMenu = document.getElementById ("nav-menu");
-	
-	menuButton.addEventListener("click", toggleMenu);
-	
-	
-	let toggle = false; //hidden at first
-	function toggleMenu() {
-		if (toggle) { //if menu is visible
-		navMenu.classList.remove("displaymenu"), //hide the menu
-		toggle = false
-		}
-		else { // false: it's hidden
-		navMenu.classList.add("displaymenu"), // show the menu
-		toggle = true
-		}
-	}
-
-}) ();
-
-
-(function(){
-
-  let myName = document.getElementById("my-name");
-  let getName = document.getElementById("get-name");
-  let userName = document.getElementById("user-name");
-  let clearStorage = document.getElementById("clearStorage");
-  let nameStored = localStorage.name;
-  let chosenColour = document.getElementById('colorPicker');
-  let colourStored = localStorage.colour;
-
-  // Displays the name in the console at this stage:
-  console.log(`Name on page load: ${nameStored}`);
-
-  if(nameStored) {
-    // If there's a name in localStorage, use it:
-    myName.innerHTML = `again ${nameStored}`;
-    console.log(`Name stored is: ${nameStored}`);
-  }
-  else {
-    // There's no name in localStorage:
-    myName.innerHTML = "stranger";
-    console.log(`No name stored`);
-  }
-  
-  if(colourStored) {
-		document.body.style.backgroundColor = colourStored;
-	}
-
-  function clearStorageFunc(){
-    localStorage.clear();
-  }
-
-  function changeBGColour(){
-    document.body.style.backgroundColor = chosenColour.value;
-	colourStored = chosenColour.value;
-	localStorage.colour = colourStored;	
-  }
-  
-
-  function PerformGreeting() {
-    if(userName.value === "") {
-      alert("Please enter a name");
-      userName.focus();
-    }
-    // Gets the name the user entered:
-    nameStored = userName.value;
-
-    // Shows the name in "my-name":
-    myName.innerHTML = nameStored;
-
-    // Puts the name into localStorage:
-    localStorage.name = nameStored;
-
-    // Displays the name in the console at the final stage:
-    console.log(`New name stored: ${nameStored}`);
-
-    return false;
-  }
-
-  if (typeof event === "undefined") {
-    getName.onsubmit = PerformGreeting; // for Firefox
-  }
-  else {
-    getName.addEventListener("submit", PerformGreeting);
-    event.preventDefault();
-  }
-
-  clearStorage.addEventListener("click",clearStorageFunc);
-
-  colorPicker.addEventListener("change",changeBGColour);
-
-}());
-
-
-function initMap() {
-  let leicester = {lat: 52.629109, lng: -1.139164};
-  
-  let mapDemo = document.getElementById("map");
-
-  // this sets the default location for when the map is first loaded
-  let map = new google.maps.Map(mapDemo, {
-    zoom: 13,
-    center: leicester
-  });
-
-  // these set different markers you want to show on your map
-  let markerLeicester = new google.maps.Marker({
-    position: leicester,
-    map: map,
-    title: 'Kimberlin Library'
-  });
-  
-}
-
 "use strict";
 
 (function(){
@@ -159,7 +42,7 @@ https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&pro
     // initialise some variables
     let theData = "";
     let langLinks = "";
-    let img = "<img>";
+    let wikiimg = "<wikiimg>";
     const languages = ["en", "de", "zh", "fr", "es", "ja", "ar", "ko", "el"];
     let k;
     let key;
@@ -167,7 +50,7 @@ https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&pro
     for(key in data.query.pages) {
       let tmp = data.query.pages[key];
       if (tmp.thumbnail) {
-        img = `<img src="${tmp.thumbnail.source}" alt="${tmp.title}"> `;
+        wikiimg = `<wikiimg src="${tmp.thumbnail.source}" alt="${tmp.title}"> `;
       }
       let title = `<strong><a href="${tmp.fullurl}">${tmp.title}</a></strong>`;
       let extract = `<span class="txt">${tmp.extract}</span>`;
@@ -177,7 +60,7 @@ https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&pro
           langLinks += `<a href=${tmp.langlinks[k].url}>${tmp.langlinks[k].lang}</a> `;
         }
       }
-      theData += `<li>${img} ${title} ${extract} <span class="langs">${langLinks}</span></li>`;
+      theData += `<li>${wikiimg} ${title} ${extract} <span class="langs">${langLinks}</span></li>`;
     }
     demoJSON.innerHTML = theData;
   }
@@ -208,17 +91,3 @@ https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&pro
   }, false);
 
 }());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
